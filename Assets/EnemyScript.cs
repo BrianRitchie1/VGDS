17da3ce359882d5b64cbbd56bc6c218dda307e6f
CAdamWebb <131796333+CAdamWebb@users.noreply.github.com>
2023-06-23 18:58:50 -0600

    Enemies

---
+++ Assets/EnemyScript.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+
+    public int maxHealth;
+    private int health;
+    private Rigidbody2D rb;
+    private Rigidbody2D player;
+    public float speed;
+    public float jump;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float r = Random.Range(0,1000);
+        Debug.Log(r);
+        if (r <= 3)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jump);
+        }
+
+
+
+
+        if (player.position.x > rb.position.x)
+        {
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+        }
+    }
+        void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            Destroy(collision.gameObject);
+            health -= 1;
+            if (health <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
